c62dfe002774a489d2933fa2102ead9ea25c971e
Nisansala <sumudunisansala999@gmail.com>
2025-01-05 18:10:53 +0530

    Absolute with relative positioning ✔️

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom';
 
 export default function LoginPage() {
     return (
-        <div className='w-[359px] h-[359px] border border-black'>
-            <div className='w-36 h-36 bg-blue-600'>
+        <div className='w-[359px] h-[359px] border border-black relative'>
+            <div className='w-36 h-36 bg-blue-600 absolute right-[10px] bottom-[10px] z-100'>
             </div>
             <div className='w-36 h-36 bg-yellow-600 fixed right-[10px] bottom-[10px]'>
             </div>
