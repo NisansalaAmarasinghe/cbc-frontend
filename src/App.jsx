8a2cef178f33b64046166185f4a0cd14bd8eaae0
Nisansala <sumudunisansala999@gmail.com>
2025-01-05 21:23:29 +0530

    Change Background Color✨

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,8 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className='bg-red-200 h-[100vh]'>
+  
       <BrowserRouter>
       
       <Routes path="/*">
@@ -20,7 +21,7 @@ function App() {
       </Routes>
     
       </BrowserRouter>
-    </>
+    </div>
   )
 }
 
