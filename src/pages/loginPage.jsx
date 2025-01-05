365ae71ad87d4a9a1bd82f78efc28327ba1e06d3
Nisansala <sumudunisansala999@gmail.com>
2025-01-05 17:39:59 +0530

    Remove all css files

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -7,7 +7,7 @@ export default function LoginPage(){
             <h1>Login Page</h1>
             <input type="text" placeholder="Enter your username"/>
             <input type="password" placeholder="Enter your password"/>
-            <button className='b'>Login</button>
+            <button className='bg-yellow-500 p-10'>Login</button>
             
             <Link to="/">Home</Link>
         </div>
