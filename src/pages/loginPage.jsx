6e80b1c3af8467e3699b42a39f770e081a84ea9e
Nisansala <sumudunisansala999@gmail.com>
2025-01-05 17:47:03 +0530

    Margin VS Padding 📑

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -7,6 +7,7 @@ export default function LoginPage(){
             <h1>Login Page</h1>
             <input type="text" placeholder="Enter your username"/>
             <input type="password" placeholder="Enter your password"/>
+            <button className='bg-yellow-500 m-10'>Login</button>
             <button className='bg-yellow-500 p-10'>Login</button>
             
             <Link to="/">Home</Link>
