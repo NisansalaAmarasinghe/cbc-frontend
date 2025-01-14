1f42a0b78476407bdc34573545d183126de4e3a6
Nisansala <sumudunisansala999@gmail.com>
2025-01-14 11:07:44 +0530

    Retrieve Product List 📃

--- src/pages/admin/adminProductsPage.jsx
+++ src/pages/admin/adminProductsPage.jsx
@@ -1,7 +1,49 @@
 import axios from "axios"
+import { useState } from "react"
 export default function AdminProductsPage(){
 
-    getProducts()
+    const[products,setProducts]=useState([
+        [
+            {
+                "_id": "67824ea6b51170e459cdae5d",
+                "productId": "B1001",
+                "productName": "Hydrating face serum",
+                "altNames": [
+                    "Moisturizing Serum",
+                    "Glow Boast Serum"
+                ],
+                "images": [
+                    "https://example.com/images/serum1.jpg",
+                    "https://example.com/images/serum.alt.jpg"
+                ],
+                "price": 24.99,
+                "lastPrice": 200,
+                "stock": 200,
+                "description": "A lightweight, fastabsorbing serum infused with hydraunic acid and vitamin C to hydrate abd brighten your skin",
+                "__v": 0
+            },
+            {
+                "_id": "6785f55104694ec146f96c1c",
+                "productId": "B1002",
+                "productName": "Hydrating face Mask",
+                "altNames": [
+                    "Moisturizing Mask",
+                    "Glow Boast Serum"
+                ],
+                "images": [
+                    "https://example.com/images/serum1.jpg",
+                    "https://example.com/images/serum.alt.jpg"
+                ],
+                "price": 28.99,
+                "lastPrice": 300,
+                "stock": 100,
+                "description": "A lightweight, fastabsorbing serum infused with hydraunic acid and vitamin C to hydrate abd brighten your skin",
+                "__v": 0
+            }
+        ]
+    ])
+
+    console.log(products)
 
     return(
         <div>
