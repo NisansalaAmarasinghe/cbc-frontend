4d74e1febed706afdb5794354e6f5fba370022ec
Nisansala <sumudunisansala999@gmail.com>
2025-01-19 21:33:20 +0530

    Supabase Connectivity 🖇️

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,4 +1,10 @@
+import { createClient } from "@supabase/supabase-js"
 import { useState } from "react"
+
+const key = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxvdnpncHdwZ3d3eGdwdGVncGRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY5NTI4MTAsImV4cCI6MjA1MjUyODgxMH0.15PoLZkrQftS5j-EywhtLMIW-_C-OhfQMPbeLqz-fs8`
+
+
+const url = "https://lovzgpwpgwwxgptegpdl.supabase.co"
 
 export default function FileUploadTest(){
 
@@ -7,10 +13,36 @@ export default function FileUploadTest(){
     function handleUpload(){
         if(file==null){
             alert("please select a file")
+            return
         }
 
         console.log(file)
+
+        //check file type and split
+        const fileName = file.name
+        const extension = fileName.split(".")[fileName.split(".").length-1]
+
+        if(extension != "jpg" && extension != "png"){
+            alert("Please select a jpg or png file")
+            return
+        }
+
+        //to connect supabase
+        const supabase = createClient(url,key)
+
+        supabase.storage.from("images").upload(file.name,
+            file,{
+                cacheControl : "3600",
+                upsert : false
+            }).then((res)=>{
+                console.log(res)
+            })
+
+        supabase.storage.from
+
+        
     }
+
     return(
         <div>
             <h1>File Uplaod Test</h1>
